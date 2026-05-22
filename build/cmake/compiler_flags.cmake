libsdl-org/aom

commit 1f9bbefce44d1eec5a963092f3033f39c1feedf0

cmake: don't use CMAKE_CONFIGURATION_TYPES. It's not defined for single-configuration CMake generators.

build/cmake/compiler_flags.cmake
@@ -71,10 +71,10 @@ function(add_c_flag_if_supported c_flag)
 
   if(${C_FLAG_SUPPORTED})
     append_flag(AOM_C_FLAGS "${c_flag}")
-    foreach(config ${CMAKE_CONFIGURATION_TYPES})
+    #foreach(config ${CMAKE_CONFIGURATION_TYPES})
       unset(C_FLAG_FOUND)
-      add_compile_options($<$<AND:$<COMPILE_LANGUAGE:C>,$<CONFIG:${config}>>:${c_flag}>)
-    endforeach()
+      add_compile_options($<$<COMPILE_LANGUAGE:C>:${c_flag}>)
+    #endforeach()
   else()
     append_flag(AOM_FAILED_C_FLAGS "${c_flag}")
   endif()
@@ -107,10 +107,10 @@ function(add_cxx_flag_if_supported cxx_flag)
 
   if(${CXX_FLAG_SUPPORTED})
     append_flag(AOM_CXX_FLAGS "${cxx_flag}")
-    foreach(config ${CMAKE_CONFIGURATION_TYPES})
+    #foreach(config ${CMAKE_CONFIGURATION_TYPES})
       unset(CXX_FLAG_FOUND)
-      add_compile_options($<$<AND:$<COMPILE_LANGUAGE:CXX>,$<CONFIG:${config}>>:${cxx_flag}>)
-    endforeach()
+      add_compile_options($<$<COMPILE_LANGUAGE:CXX>>:${cxx_flag}>)
+    #endforeach()
   else()
     append_flag(AOM_FAILED_CXX_FLAGS "${cxx_flag}")
   endif()

commit da21ef4e1b66d4dba9c59ce28dff68a456b6fd89

cmake: fix typo introduced by 1f9bbefce44d1eec5a963092f3033f39c1feedf0

build/cmake/compiler_flags.cmake
@@ -109,7 +109,7 @@ function(add_cxx_flag_if_supported cxx_flag)
     append_flag(AOM_CXX_FLAGS "${cxx_flag}")
     #foreach(config ${CMAKE_CONFIGURATION_TYPES})
       unset(CXX_FLAG_FOUND)
-      add_compile_options($<$<COMPILE_LANGUAGE:CXX>>:${cxx_flag}>)
+      add_compile_options($<$<COMPILE_LANGUAGE:CXX>:${cxx_flag}>)
     #endforeach()
   else()
     append_flag(AOM_FAILED_CXX_FLAGS "${cxx_flag}")